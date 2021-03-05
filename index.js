441269ea004ad390c3a75f50e2955c98a449148d
davindb <davindbhagaspati@gmail.com>
2021-03-05 15:32:55 +0700

    First Program without css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+// FIRST PROGRAM
 const http = require("http");
 const fs = require("fs");
-// const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
@@ -14,13 +14,5 @@ const server = http.createServer((req, res) => {
     res.end();
   });
 });
-
-// server.listen(port, (err) => {
-//   if (err) {
-//     console.log("Something went wrong.");
-//   } else {
-//     console.log(`Server is listening on port ${port}`);
-//   }
-// });
 
 server.listen(process.env.PORT || 5000);
