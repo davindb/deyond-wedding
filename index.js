403e1bb56182d24430b1f3b5c464af1b0183eda4
davindb <davindbhagaspati@gmail.com>
2021-03-05 15:24:39 +0700

    fixing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const port = 3000;
+// const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
@@ -15,10 +15,12 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(port, (err) => {
-  if (err) {
-    console.log("Something went wrong.");
-  } else {
-    console.log(`Server is listening on port ${port}`);
-  }
-});
+// server.listen(port, (err) => {
+//   if (err) {
+//     console.log("Something went wrong.");
+//   } else {
+//     console.log(`Server is listening on port ${port}`);
+//   }
+// });
+
+server.listen(process.env.PORT || 5000);
